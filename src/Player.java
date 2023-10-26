b267b1958b639f050bcdbbee29700db3e85f2005
Brandon Wong <brandonfn2007@gmail.com>
2023-10-25 23:14:28 -0400

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,36 @@
+public class Player {
+
+
+    private String name;
+    private int score;
+    private int winStreak;
+
+
+    public Player(String name) {
+        this.name = name;
+        score = 0;
+        winStreak = 0;
+    }
+
+
+    public int getScore() {
+        return score;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void incrementScore() {
+        score++;
+    }
+
+
+    public void reset() {
+        score = 0;
+    }
+    public int getWinStreak() { return winStreak; }
+    public void incrementWinStreak() { winStreak++; }
+}
